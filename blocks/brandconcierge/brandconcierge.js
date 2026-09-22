fffccacf5bbe196a36de7d78627971c4c0a68065
amitpanigrahi <48883128+amitpanigrahi@users.noreply.github.com>
2026-09-22 17:22:48 +0530

    Modify brandconcierge.js to include innerHTML setup
    Updated the block to set innerHTML with a div and script for Adobe Concierge.

--- blocks/brandconcierge/brandconcierge.js
+++ blocks/brandconcierge/brandconcierge.js
@@ -1,3 +1,14 @@
 export default function decorate(block) {
-  block.setAttribute('id', 'brand-concierge-mount');
+  //block.setAttribute('id', 'brand-concierge-mount');
+  block.innerHTML = `
+     <div id="brand-concierge-mount"></div>
+      <script>
+        window.adobe.concierge.bootstrap({
+          instanceName: "alloy",
+          stylingConfigurations: window.styleConfiguration,
+          selector: "#brand-concierge-mount",
+          stickySession: false
+        });
+      </script>
+  `;
 }
