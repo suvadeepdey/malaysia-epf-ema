554ce23c81b934cad9811bdaf6f84d79d4cc71a5
amitpanigrahi <48883128+amitpanigrahi@users.noreply.github.com>
2026-09-22 17:35:40 +0530

    Refactor brandconcierge.js to streamline bootstrap call

--- blocks/brandconcierge/brandconcierge.js
+++ blocks/brandconcierge/brandconcierge.js
@@ -1,14 +1,9 @@
 export default function decorate(block) {
-  //block.setAttribute('id', 'brand-concierge-mount');
-  block.innerHTML = `
-     <div id="brand-concierge-mount"></div>
-      <script>
-        window.adobe.concierge.bootstrap({
-          instanceName: "alloy",
-          stylingConfigurations: window.styleConfiguration,
-          selector: "#brand-concierge-mount",
-          stickySession: false
-        });
-      </script>
-  `;
+  block.setAttribute('id', 'brand-concierge-mount');
+  window.adobe.concierge.bootstrap({
+      instanceName: "alloy",
+      stylingConfigurations: window.styleConfiguration,
+      selector: "#brand-concierge-mount",
+      stickySession: true
+  });
 }
